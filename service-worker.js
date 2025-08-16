30dc4635fba007ad03541128deade713755cf267
geibeltanner-dawgcheck <geibeltanner@gmail.com>
2025-08-16 07:57:59 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,25 @@
-// Simple cache for trainer
-const CACHE = 'moo-cache-v5';
+const CACHE = 'moo-trainer-v4';
 const ASSETS = [
+  './',
   './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
+  './styles.css?v=4',
+  './app.js?v=4',
+  './manifest.json?v=4',
+  './icons/logo.png',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
-
-self.addEventListener('activate', e => {
+self.addEventListener('activate', e=>{
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null)))
   );
 });
-
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', e=>{
   e.respondWith(
-    caches.match(e.request, { ignoreSearch:true })
-      .then(r => r || fetch(e.request))
+    caches.match(e.request, {ignoreSearch:true}).then(r=> r || fetch(e.request))
   );
 });
