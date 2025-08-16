91f4963e83fd831978dea120a9776f4ea8f827b7
geibeltanner-dawgcheck <geibeltanner@gmail.com>
2025-08-16 14:52:29 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
-const CACHE = 'moo-sim-v1';
+// DAWGCHECK trainer service worker (static cache)
+const CACHE = 'dawgcheck-v1';
 const ASSETS = [
   './',
   './index.html',
@@ -9,18 +10,22 @@ const ASSETS = [
   './icons/icon-512.png'
 ];
 
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
 });
 
-self.addEventListener('activate', e=>{
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
   );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', e=>{
+self.addEventListener('fetch', e => {
+  const req = e.request;
   e.respondWith(
-    caches.match(e.request, {ignoreSearch:true}).then(res=>res||fetch(e.request))
+    caches.match(req).then(cached => cached || fetch(req))
   );
 });
