a70182d2d89b1e6e210cc1bd4ea38703ac4564f3
geibeltanner-dawgcheck <geibeltanner@gmail.com>
2025-08-16 00:47:42 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,28 @@
-const CACHE = "moo-cache-v1";
+// Simple offline cache
+const CACHE = 'moo-cache-v1';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  './',
+  './index.html',
+  './style.css',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
-self.addEventListener("install", e => {
+self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request, { ignoreSearch: true })
+      .then(r => r || fetch(e.request))
   );
 });
