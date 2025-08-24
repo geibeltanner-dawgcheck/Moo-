750f8a03e91f342b606f92cfb2d40c34e425252b
geibeltanner-dawgcheck <geibeltanner@gmail.com>
2025-08-24 02:24:23 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,25 @@
-// DAWGCHECK trainer service worker (static cache)
-const CACHE = 'dawgcheck-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './app.js',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+const CACHE="dawgtrainer-v5";
+const ASSETS=[
+  "./","./index.html","./styles.css","./app.js","./manifest.json",
+  "./assets/icons/icon-192.png","./assets/icons/icon-512.png","./assets/city.jpg"
 ];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
   self.skipWaiting();
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-  );
+self.addEventListener("activate",e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
   self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
-  const req = e.request;
-  e.respondWith(
-    caches.match(req).then(cached => cached || fetch(req))
-  );
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET")return;
+  if(e.request.destination==="document"){
+    e.respondWith(fetch(e.request).then(r=>{
+      caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r;
+    }).catch(()=>caches.match(e.request)));
+  }else{
+    e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{
+      caches.open(CACHE).then(cc=>cc.put(e.request,r.clone()));return r;
+    })));
+  }
 });
